cd085bb91d86d20ed501bb8baf0190313f27ac7c
LuigiPaolino <luigi.pl@gmail.com>
2023-08-20 21:27:22 -0500

    Initial Commit

---
+++ src/main/java/com/shopcart/items/ShopCartApiItemsApplication.java
@@ -0,0 +1,13 @@
+package com.shopcart.items;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopCartApiItemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopCartApiItemsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shopcart/items/ShopCartApiItemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopcart.items;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopCartApiItemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
